79405d318bfbb4d2d3d66581feff5bd9c270822f
Carlin23-stack <carlin1013799@students.sbcsc.k12.in.us>
2026-09-25 12:23:53 -0400

    Fix Retro Bowl embed URL

--- games.js
+++ games.js
@@ -1,1 +1,1 @@
-const games=[{title:'Retro Bowl',description:'Play football and build your dynasty.',category:'Sports',icon:'🏈',colors:['#ee8b45','#b93767'],iframeUrl:'https://retro-bowl-six.vercel.app'}];
+const games=[{title:'Retro Bowl',description:'Play football and build your dynasty.',category:'Sports',icon:'🏈',colors:['#ee8b45','#b93767'],iframeUrl:'https://retro-bowl-clone.vercel.app/'}];
