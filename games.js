6dd9fbac22ca07ff56ceef583fc5f93ae8521d6e
Carlin23-stack <carlin1013799@students.sbcsc.k12.in.us>
2026-09-25 12:25:04 -0400

    Use direct Kongregate Retro Bowl iframe URL

--- games.js
+++ games.js
@@ -1,1 +1,1 @@
-const games=[{title:'Retro Bowl',description:'Play football and build your dynasty.',category:'Sports',icon:'🏈',colors:['#ee8b45','#b93767'],iframeUrl:'https://retro-bowl-clone.vercel.app/'}];
+const games=[{title:'Retro Bowl',description:'Play football and build your dynasty.',category:'Sports',icon:'🏈',colors:['#ee8b45','#b93767'],iframeUrl:'https://game316009.konggames.com/gamez/0031/6009/live/index.html'}];
